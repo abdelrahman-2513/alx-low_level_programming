c3db0b9917b7afc31aa44a3715502a2003b65838
abdelrahman-2513 <ab19622001@gmail.com>
2023-07-18 10:39:50 -0700

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - if the number is negative it print out absolute
+ * @n: the int to checked
+ * Return: the absolute value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
