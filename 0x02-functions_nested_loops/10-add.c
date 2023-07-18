b3d6bca28a2d76d3e391dd1a83e6af861a209d34
abdelrahman-2513 <ab19622001@gmail.com>
2023-07-18 13:14:55 -0700

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - take two parameters and return the sum of them
+ * @a: first number to be added
+ * @b: second number to be added
+ * Return: the sum of a & b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
