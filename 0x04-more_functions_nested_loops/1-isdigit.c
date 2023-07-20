6c9502c000ccf1ab92c04558ebf1e3ffb040381b
abdelrahman-2513 <ab19622001@gmail.com>
2023-07-20 07:36:04 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - is used to check that the char enterd is a digit
+ * @c: the character to be checked
+ *
+ * Return: char is digit 1 else 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
