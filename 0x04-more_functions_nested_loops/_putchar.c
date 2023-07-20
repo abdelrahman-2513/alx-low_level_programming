da5953bcffcd617f0c2ddf97a8fa83706c8b5483
abdelrahman-2513 <ab19622001@gmail.com>
2023-07-20 07:30:49 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - write the character using stdout
+ * @c: the character to be printed
+ *
+ * Return: on access 1
+ * onerror -1
+ */
+int _putchar(int c)
+{
+	return (write(1, &c, 1));
+}
