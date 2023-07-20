013dde7ebf4ffbe105c2170755c5dcdd2ee101f1
abdelrahman-2513 <ab19622001@gmail.com>
2023-07-20 09:18:36 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints from 1 -  100 if the number divisble by 3 FUZZ
+ * if divisible by 5 BUZZ if both FIZZBUZZ
+ * Return: always return 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf(i);
+		}
+		printf(" ");
+	}
+}
